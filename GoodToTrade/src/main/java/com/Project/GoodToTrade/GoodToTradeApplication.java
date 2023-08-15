5870b93d92db2c3b5c5306fb067abafe86419af5
JReuman1 <99495156+JReuman1@users.noreply.github.com>
2023-08-15 12:42:25 +0200

    Change some features

--- GoodToTrade/src/main/java/com/Project/GoodToTrade/GoodToTradeApplication.java
+++ GoodToTrade/src/main/java/com/Project/GoodToTrade/GoodToTradeApplication.java
@@ -16,32 +16,31 @@ import java.util.ArrayList;
 @SpringBootApplication
 public class GoodToTradeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GoodToTradeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GoodToTradeApplication.class, args);
+    }
 
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-	}
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
-	@Bean
-	CommandLineRunner run(UsersService usersService) {
-		return args -> {
-			usersService.saveRole(new Role(null, "ROLE_USER"));
-			usersService.saveRole(new Role(null, "ROLE_ADMIN"));
+    @Bean
+    CommandLineRunner run(UsersService usersService) {
+        return args -> {
+            usersService.saveRole(new Role(null, "ROLE_USER"));
+            usersService.saveRole(new Role(null, "ROLE_ADMIN"));
 
-			usersService.saveUser(new Users("Steinburg", "Javier Esparza", "cof.core@gmail.com", "123456789", "1234", new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
-			usersService.saveUser(new Users("James", "Chicorita", "james@smith.com", "123456789", "1234", new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
-			usersService.saveUser(new Users("Jane", "Pikachu", "jane@carry.com", "123456789", "1234", new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
-			usersService.saveUser(new Users("Chris", "Corleone", "chris@anderson.com", "123456789", "1234", new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
+            usersService.saveUser(new Users("Steinburg", "Javier Esparza", "cof.core@gmail.com", "123456789", "1234", new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
+            usersService.saveUser(new Users("James", "Chicorita", "james@smith.com", "123456789", "1234", new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
+            usersService.saveUser(new Users("Jane", "Pikachu", "jane@carry.com", "123456789", "1234", new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
+            usersService.saveUser(new Users("Chris", "Corleone", "chris@anderson.com", "123456789", "1234", new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
 
-			usersService.addRoleToUser("Steinburg", "ROLE_ADMIN");
-			usersService.addRoleToUser("james", "ROLE_ADMIN");
-			usersService.addRoleToUser("jane", "ROLE_USER");
-			usersService.addRoleToUser("chris", "ROLE_ADMIN");
-			usersService.addRoleToUser("chris", "ROLE_USER");
-		};
-	}
-
+            usersService.addRoleToUser("Steinburg", "ROLE_ADMIN");
+            usersService.addRoleToUser("James", "ROLE_ADMIN"); // Cambiado 'james' a 'James'
+            usersService.addRoleToUser("Jane", "ROLE_USER");   // Cambiado 'jane' a 'Jane'
+            usersService.addRoleToUser("Chris", "ROLE_ADMIN"); // Cambiado 'chris' a 'Chris'
+            usersService.addRoleToUser("Chris", "ROLE_USER");  // Cambiado 'chris' a 'Chris'
+        };
+    }
 }
